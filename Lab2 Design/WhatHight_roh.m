c213efd0e4790df7ac1a021e2fc4140393e31c49
Abdullah <iAbdullah.AlAmeri@gmail.com>
2018-10-08 12:09:29 -0600

    Fixed the What hight function

--- Lab2 Design/WhatHight_roh.m
+++ Lab2 Design/WhatHight_roh.m
@@ -29,13 +29,12 @@ function h = WhatHight_roh(roh)
 % OUTPUTS:
 %           - Hight in m.
 
-roh_air = 0;
+roh_air = 10;
 i = 0;
 
-while abs(roh_air - roh) >= 7
-    %sit the condition until we get a very close match between the two densities.
-    %The reason that this number is high is to avoid user mistakes,
-    %although 7 isn't a really big difference compared to ths units.
+while abs(roh_air - roh) >= 1e-4
+    %the more you want to be accurate with hight you'll change the
+    %conditioon, however your input hight must be more accurate too!
     
     %Unless the density is inputted to a very high degree of accuracy
     %relative to how many sig figs, this function need some bigger
@@ -45,7 +44,7 @@ while abs(roh_air - roh) >= 7
    
     [ a b c d ] = atmosisa(i); %we get our data @ each hight.
     
-    roh_air = c; %the output named c corresponds to the density of the air.
+    roh_air = d; %the output named c corresponds to the density of the air.
     
 end
 
