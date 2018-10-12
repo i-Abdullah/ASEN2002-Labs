9a9409b34c4194e91aa581e0add8365b6f4d962e
Abdullah <iAbdullah.AlAmeri@gmail.com>
2018-10-12 12:42:36 -0600

    Code Updated

--- Lab2 Design/Ballon.m
+++ Lab2 Design/Ballon.m
@@ -5,13 +5,14 @@ close all;
 
 %% Define initial Values
 
-MassLoad = 500 %kg
-SafetyFactor = 3.5; %Safety Values
+MassLoad = 500; %kg
+SafetyFactor = 2; %Safety Values
 GagePressure = 10; % 10 pascals
-MueU = 199947961.5 ; %Ultimate Tensile Strength Pa.
-RGas = 2.0769 ; %Gas constant
+MueU = 199947961.5; %Ultimate Tensile Strength Pa.
+RGas = 2.0769; %Gas constant
 StevBoltzConst = 5.670e-8; % Stevents Boltzman Constant
-DensityMylar = 1350 % kg/m^3 
+DensityMylar = 1350; % kg/m^3 
+HeliumSeaLevel = 0.164;
 
 %-=-=-=-=-=-=-=-=-=-=-=( Heat transfer )=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -19,24 +20,28 @@ DensityMylar = 1350 % kg/m^3
 
 % TEH SUN:
 
-AbsroptivitySun = 0.6
+AbsroptivitySun = 0.6;
 qSun = 1370; % W / m^2 
 
 % Albedo
 
 qEarth = 237; % W / m^2
+AbsroptivityEarth = 0.8;
 
 % Emissivity of Ballon
 
 EmissivityMaterial = 0.8;
 
 
-%% Setup Equlibirum eqaution at the new hight
+%% Setup Equlibirum eqaution at the day and night
 
 %-=-=-=-=-=-=-=-=-=-=-=( Find New T )=-=-=-=-=-=-=-=-=-=-=-=
 
-T_New = ((( EmissivityMaterial * qEarth )/2 + (AbsroptivitySun*qSun)/2) / (4*EmissivityMaterial*StevBoltzConst))^(1/4);
+T_New_Day = ((( EmissivityMaterial * qEarth )/2 + (AbsroptivitySun*qSun)/2) / (4*EmissivityMaterial*StevBoltzConst))^(1/4);
+T_New_Night = ((qEarth*AbsroptivityEarth*1/2)/(EmissivityMaterial*StevBoltzConst))^(1/4)
 
+
+%% 
 %-=-=-=-=-=-=-=-=-=-=-=( Find raduis, and Gas Density )-=-=-=-=-=-=-=-=-=-=
 
 % We will use a complex loop to first find the total new density, we will
@@ -53,11 +58,15 @@ height = 35000;
 [ TLoop aLoop PLoop rhoLoop ] = atmoscoesa(height);
 
 %New Density via Ideal Gas law ( 1 / Specific Volume).
-NewDensityGas = ( ((PLoop+10)/1000) / (RGas*T_New) );
+NewDensityGas = ( ((PLoop+10)/1000) / (RGas*T_New_Night) );
 
 % Solve the force Balance equation for the raduis of ballon
-RaduisCuibed = MassLoad / ( (4*pi/3)  * ( rhoLoop - NewDensityGas - ( 3 * DensityMylar * ( (GagePressure * SafetyFactor) / (2*MueU) ) ) ) )
+RaduisCuibed = MassLoad / ( (4*pi/3)  * ( rhoLoop - NewDensityGas - ( 3 * DensityMylar * ( (GagePressure * SafetyFactor) / (2*MueU) ) ) ) );
 Raduis = RaduisCuibed^(1/3);
+
+VolumeBal = (4/3)*pi*RaduisCuibed;
+massOfHelium = VolumeBal*NewDensityGas;
+volumeSeaLevel = massOfHelium*HeliumSeaLevel;
 
 %-=-=-=-=-=-=-=-=-=-=-=( Find Density of Ballon )=-=-=-=-=-=-=-=-=-=-=-=
 
