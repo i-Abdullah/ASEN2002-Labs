53c12ff90ed3fdc1c38c93ffff9425b06dda3d35
Abdullah <iAbdullah.AlAmeri@gmail.com>
2018-10-24 09:18:31 -0600

    We can read the data

--- Lab 3 Wind Tunnel/WindTunnel.m
+++ Lab 3 Wind Tunnel/WindTunnel.m
@@ -25,3 +25,39 @@ clc;
 close all;
 
 %% read the data
+
+%write the file names.
+
+filename_VV = 'VelocityVoltage_S011_G01.csv'; %the vleocity voltage file name
+filename_BL = 'BoundaryLayer_S011_G01.csv'; %the Boudnary layer file name
+
+
+%read
+
+data_VV = csvread(filename_VV,1,0);
+data_BL = csvread(filename_BL,1,0);
+
+% divide, and conquer.
+
+%from Velocity Voltage data
+atm_P_VV = data_VV(:,1); %Atmospheric pressure in Pa.
+atm_Temp_VV = data_VV(:,2); %Atmospheric Temperature in K.
+air_diff_P_VV = data_VV(:,3); %Aire pressure Differntial in Pa.
+Aux_diff_P_VV = data_VV(:,4); %Auxillery pressure Differntial in Pa.
+Eld_x_VV = data_VV(:,5); %ELD Probe x axis location in mm.
+Eld_y_VV = data_VV(:,6); %ELD Probe y axis location in mm.
+Voltage_VV = data_VV(:,7); % Voltage data were recorded at (in Volts).
+
+%frm Boundary layer
+
+atm_P_BB = data_BL(:,1); %Atmospheric pressure in Pa.
+atm_Temp_BB = data_BL(:,2); %Atmospheric Temperature in K.
+air_diff_P_BB = data_BL(:,3); %Aire pressure Differntial in Pa.
+Aux_diff_P_BB = data_BL(:,4); %Auxillery pressure Differntial in Pa.
+Eld_x_BB = data_BL(:,5); %ELD Probe x axis location in mm.
+Eld_y_BB = data_BL(:,6); %ELD Probe y axis location in mm.
+Voltage_BB = data_BL(:,7); % Voltage data were recorded at (in Volts).
+
+%%
+
+
