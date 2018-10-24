cb9aaf1e307864e9533b4f023d610dc768401183
Abdullah <iAbdullah.AlAmeri@gmail.com>
2018-10-24 09:04:41 -0600

    Started the code, and uploaded the data files

---
+++ Lab 3 Wind Tunnel/WindTunnel.m
@@ -0,0 +1,27 @@
+%% info
+
+%{
+
+Thie script will extract and analyze data obtained from a wind tunnel lab,
+part of ASEN 2002: Lab 3, CU Bouldr, Fall 18.
+
+
+Done by
+
+1- Jack Soltys
+2- Abdulla Al Ameri
+3- Greer Foster
+4- Caelan Maitland
+
+
+%}
+
+
+
+%% housekeeping
+
+clear;
+clc;
+close all;
+
+%% read the data
