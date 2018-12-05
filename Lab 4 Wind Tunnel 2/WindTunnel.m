f38e1c5ab0899954218b808960708f8922068dce
Abdullah <iAbdullah.AlAmeri@gmail.com>
2018-12-05 07:50:40 -0700

    Graphing coeff of pressure

--- Lab 4 Wind Tunnel 2/WindTunnel.m
+++ Lab 4 Wind Tunnel 2/WindTunnel.m
@@ -44,6 +44,8 @@ stingNormForce = Data(:,24);
 stingAxialForce = Data(:,25);
 stingPitchingMoment = Data(:,26);
 
+
+
 %% Geometry of airfoil
 
 AirfoilGeometry = xlsread('Data/AirfoilGeometry.xlsx',1);
@@ -186,64 +188,3 @@ hold off;
 end
 
 a = 1;
-%% Extrapolate pressure:
-
-%Explain:
-
-%{
-
-To do this we can extrapolate the pressure at the trailing edge (x = 3.5in)
-based upon  pressure measured at the last two ports on both the upper and
-lower wing surfaces (upper: p10, p8 and lower: p12, p14. These linear
-extrapolations will give us two  different  estimates for the pressure
-at the trailing edge, but we know that the pressures from the upper and
-lower surfaces must converge at the trailing edge. Thus we should average
-these  two pressure estimates to provide one estimate at the trailing  edge. 
-
-%}
-
-%Take Mean Values and linearly extrapolate.
-%each angel of attack has 3 speeds
-
-SP8_location = 2.1; %in m
-SP10_location = 2.8; %in m
-SP12_location = 2.8; %in m
-SP14_location = 2.1; %in m
-
-Ptrail = 0;
-
-LocationTrail = 3.5; %where the trail is in meters.
-
-%each two conscutive ports on the same height will be in the same loop.
-%so 10 and 8 together, and 12 and 14 together.
-
-% SP 8 AND SP 10
-for i=1:1:9
-y = [ mean(SP8((i*20-19):i*20)); mean(SP10((i*20-19):i*20)) ];
-t = [ SP8_location; SP10_location ];
-
-Slope = (y(2)-y(1))/(t(2)-t(1));
-Intercept = y(1) - Slope*t(1) ;
-
-Ptrail_UpperPorts(i) = Slope*(LocationTrail) + Intercept;
-%Uncertinity_Ptrail_Upper(i) = sqrt( [ LocationTrail 1] * Q * [ LocationTrail ; 1 ] );
-
-end
-
-for i=1:1:9
-y = [ mean(SP12((i*20-19):i*20)); mean(SP14((i*20-19):i*20)) ];
-t = [ SP12_location; SP14_location ];
-
-%calculate the new pressure at the trailing edge.
-
-Ptrail_LowerPorts(i) = Slope*(LocationTrail) + Intercept;
-%Uncertinity_Ptrail_Lower(i) = sqrt( [ LocationTrail 1] * Q * [ LocationTrail ; 1 ] );
-
-Slope = (y(2)-y(1))/(t(2)-t(1));
-Intercept = y(1) - Slope*t(1) ;
-
-Ptrail_LowerPorts(i) = Slope*(LocationTrail) + Intercept;
-
-end
-
-
