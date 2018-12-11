9926a11622746a4e4db0d3ae32da73ba03a76e72
Abdullah <iAbdullah.AlAmeri@gmail.com>
2018-12-11 11:18:46 -0700

    Changed error analysis approach

--- Lab 4 Wind Tunnel 2/WindTunnel.m
+++ Lab 4 Wind Tunnel 2/WindTunnel.m
@@ -111,18 +111,39 @@ LocationTrail = 3.5; %where the trail is in meters.
 % SP 8 AND SP 10
 for i=1:1:12
 y = [ mean(SP8((i*20-19):i*20)); mean(SP10((i*20-19):i*20)) ];
+
+%get max/min values from trailing edge from upper half of the airfoil. 
+y_trail_max_upper = [ max(SP8((i*20-19):i*20)); max(SP10((i*20-19):i*20)) ];
+y_trail_min_upper = [ min(SP8((i*20-19):i*20)); min(SP10((i*20-19):i*20)) ];
+
 t = [ SP8_location; SP10_location ];
 
+%for mean values
 Slope = (y(2)-y(1))/(t(2)-t(1));
 Intercept = y(1) - Slope*t(1) ;
 
+%for max and min
+Slope_max = (y_trail_max_upper(2)-y_trail_max_upper(1))/(t(2)-t(1));
+Intercept_max = y_trail_max_upper(1) - Slope*t(1) ;
+
+Slope_min = (y_trail_min_upper(2)-y_trail_min_upper(1))/(t(2)-t(1));
+Intercept_min = y_trail_min_upper(1) - Slope*t(1) ;
+
+% get p values from linear extrapolation.
 Ptrail_UpperPorts(i) = Slope*(LocationTrail) + Intercept;
+Ptrail_UpperPorts_max(i) = Slope_max*(LocationTrail) + Intercept_max;
+Ptrail_UpperPorts_min(i) = Slope_min*(LocationTrail) + Intercept_min;
+
 %Uncertinity_Ptrail_Upper(i) = sqrt( [ LocationTrail 1] * Q * [ LocationTrail ; 1 ] );
 
 end
 
 for i=1:1:12
-y = [ mean(SP12((i*20-19):i*20)); mean(SP14((i*20-19):i*20)) ];
+y = [ mean(SP12((i*20-19):i*20)); mean(SP14((i*20-19):i*20))];
+%get max/min values from trailing edge from lower half of the airfoil. 
+y_trail_max_lower = [ max(SP12((i*20-19):i*20)); max(SP14((i*20-19):i*20)) ];
+y_trail_min_lower = [ min(SP12((i*20-19):i*20)); min(SP14((i*20-19):i*20)) ];
+
 t = [ SP12_location; SP14_location ];
 
 %calculate the new pressure at the trailing edge.
@@ -130,15 +151,33 @@ t = [ SP12_location; SP14_location ];
 Ptrail_LowerPorts(i) = Slope*(LocationTrail) + Intercept;
 %Uncertinity_Ptrail_Lower(i) = sqrt( [ LocationTrail 1] * Q * [ LocationTrail ; 1 ] );
 
+%for mean values
 Slope = (y(2)-y(1))/(t(2)-t(1));
 Intercept = y(1) - Slope*t(1) ;
 
+
+%for max and min
+Slope_max = (y_trail_max_lower(2)-y_trail_max_lower(1))/(t(2)-t(1));
+Intercept_max = y_trail_max_lower(1) - Slope*t(1) ;
+
+Slope_min = (y_trail_min_lower(2)-y_trail_min_lower(1))/(t(2)-t(1));
+Intercept_min = y_trail_min_lower(1) - Slope*t(1) ;
+
+
+% get p values from linear extrapolation.
 Ptrail_LowerPorts(i) = Slope*(LocationTrail) + Intercept;
+
+Ptrail_LowerPorts_max(i) = Slope_max*(LocationTrail) + Intercept_max;
+Ptrail_LowerPorts_min(i) = Slope_min*(LocationTrail) + Intercept_min;
 
 end
 
-PTrail = mean( [ Ptrail_LowerPorts ; Ptrail_LowerPorts ] );
+% get the mean of the the two values, as well as the maxes and mins.
+PTrail = mean( [ Ptrail_UpperPorts ; Ptrail_LowerPorts ] );
+PTrailmax = mean( [ Ptrail_UpperPorts_max ; Ptrail_LowerPorts_max ] );
+PTrailmin = mean([ Ptrail_UpperPorts_min ; Ptrail_LowerPorts_min ] );
 
+%%
 
 %get mean data for all ports for all angel of attacks all speeds, each
 %column should have 12 rows
@@ -146,6 +185,9 @@ PTrail = mean( [ Ptrail_LowerPorts ; Ptrail_LowerPorts ] );
 for i=1:1:12
 
 PortsMeans(i,:) = mean(Data ( ((i*20-19):(i*20)),(7:22) ),1);
+PortsMaxes(i,:) = max(Data ( ((i*20-19):(i*20)),(7:22) ));
+PortsMins(i,:) = min(Data ( ((i*20-19):(i*20)),(7:22) ));
+
 pitotDynamicPMean(i) = mean(pitotDynamicP ( ((i*20-19):(i*20)),1 ),1);
 TatmMean(i) = mean(Patm ( ((i*20-19):(i*20)),1 ),1);
 PatmMean(i) = mean(Tatm ( ((i*20-19):(i*20)),1 ),1);
@@ -158,20 +200,32 @@ end
 
 [ r c ] = size(PortsMeans);
 
+% the max and mins are for error bounds.
 SP_All_Updated = zeros(r,c+1);
+SP_All_Updated_max = zeros(r,c+1);
+SP_All_Updated_min = zeros(r,c+1);
+
 for i = 1:c+1
     if i==10
         SP_All_Updated(:,i) = PTrail;
-    elseif i<10
+        SP_All_Updated_max(:,i) = PTrailmax;
+        SP_All_Updated_min(:,i) = PTrailmin;
+
+    elseif i<10 % the 10th location is where the trailing edge is @.
     SP_All_Updated(:,i) = PortsMeans(:,i);
-    
+    SP_All_Updated_max(:,i) = PortsMaxes(:,i);
+    SP_All_Updated_min(:,i) = PortsMins(:,i);
+
     else %after the 10 is added, the index will exceed the matrix deminsions
     
      SP_All_Updated(:,i) = PortsMeans(:,i-1);
+     SP_All_Updated_max(:,i) = PortsMaxes(:,i-1);
+     SP_All_Updated_min(:,i) = PortsMins(:,i-1);
 
     end
     
 end
+
 
 a=1;
 %% Geometry of airfoil
@@ -201,6 +255,7 @@ for i=1:1:12
 PortsMeanValues(:,i) = mean(Data ( ((i*20-19):(i*20)),(7:22) ),1);
 
 % Angel of attack related to each set of columns
+
 % this will b 2 rows * N columns, the first row is angel of attack, second
 % is air speed. 
 
@@ -229,6 +284,9 @@ Qinfity = Dynamic pressure from pitot tube (@ column 5)
 for i=1:1:12
     
   Cp(:,i) = (SP_All_Updated(i,:)')./(qinf(:,i));
+  Cp_max(:,i) = (SP_All_Updated_max(i,:)')./(qinf(:,i));
+  Cp_min(:,i) = (SP_All_Updated_min(i,:)')./(qinf(:,i));
+
 
 end
 
@@ -261,7 +319,11 @@ for j=1:c
         
     Cn(i,j) = 0.5*(Cp(i,j) + Cp(i+1,j))* (dx(i)/CordLength) ;
     Ca(i,j) = 0.5*(Cp(i,j) + Cp(i+1,j))* (dy(i)/CordLength) ;
-    
+    Cn_max(i,j) = 0.5*(Cp_max(i,j) + Cp_max(i+1,j))* (dx(i)/CordLength) ;
+    Cn_min(i,j) = 0.5*(Cp_min(i,j) + Cp_min(i+1,j))* (dx(i)/CordLength) ;
+    Ca_max(i,j) = 0.5*(Cp_max(i,j) + Cp_max(i+1,j))* (dy(i)/CordLength) ;
+    Ca_min(i,j) = 0.5*(Cp_min(i,j) + Cp_min(i+1,j))* (dy(i)/CordLength) ;
+
     end
 end
 
@@ -269,11 +331,27 @@ end
 Cn = -sum(Cn,1);
 Ca = sum(Ca,1);
 
+Cn_max = -sum(Cn_max,1);
+Ca_max = sum(Ca_max,1);
+
+Cn_min = -sum(Cn_min,1);
+Ca_min = sum(Ca_min,1);
+
 %coefficient of lift and drag
 Cl = (Cn .* cosd(Alpha_V(1,:))) - (Ca .* sind(Alpha_V(1,:))) ;
 Cd = (Ca .* cosd(Alpha_V(1,:))) + (Cn .* sind(Alpha_V(1,:))) ;
 
+Cl_max = (Cn_max .* cosd(Alpha_V(1,:))) - (Ca_max .* sind(Alpha_V(1,:))) ;
+Cd_max = (Ca_max .* cosd(Alpha_V(1,:))) + (Cn_max .* sind(Alpha_V(1,:))) ;
 
+
+Cl_min = (Cn_min .* cosd(Alpha_V(1,:))) - (Ca_min .* sind(Alpha_V(1,:))) ;
+Cd_min = (Ca_min .* cosd(Alpha_V(1,:))) + (Cn_min .* sind(Alpha_V(1,:))) ;
+
+% now, we must note that the mins and maxe matrixes has the maximum values
+% and minumums for all ports, and hence, for instance, @ port 1, there's a
+% max and a min, the max is stored in one matrix, the min is stored in
+% anther, thus 
 %% plotting cp with respect to x_c
 
 
@@ -287,8 +365,17 @@ plot(x_c(:,1),zeros(1,length(Cp(:,i))),'k')
 patch([x_c(:,1), fliplr(x_c(:,1))], [Cp(:,i) fliplr(Cp(:,i))], [0.9290, 0.6940, 0.1250], 'FaceAlpha',0.8)
 alpha(0.1); %change transperancy of  the filling
 
+hold on
+%plot maximas and minmas 
+
+%maxes and mins
+errorbar(x_c(:,1),Cp(:,i),Cp_max(:,i),Cp_min(:,i))
+hold on
+
+
 grid minor
 title(['\alpha = ' num2str(Alpha_V(1,i)) ', V = ' num2str(Alpha_V(2,i))]);
+legend({'Cp Values','Linear connection','Area under the curve','Error bars'},'Orientation','horizontal','Location','SouthEast');
 xlabel( ' x/c ')
 ylabel ('Cp')
 
@@ -312,9 +399,14 @@ plot(x_c(:,1),zeros(1,length(Cp(:,i))),'k')
 patch([x_c(:,1), fliplr(x_c(:,1))], [Cp(:,i) fliplr(Cp(:,i))], [0.9290, 0.6940, 0.1250], 'FaceAlpha',0.8)
 alpha(0.1); %change transperancy of  the filling
 
+hold on
+errorbar(x_c(:,1),Cp(:,i),Cp_max(:,i),Cp_min(:,i))
+hold on
+
 grid minor
 title(['\alpha = ' num2str(Alpha_V(1,i)) ', V = ' num2str(Alpha_V(2,i))]);
 xlabel( ' x/c ')
+legend({'Cp Values','Linear connection','Area under the curve','Error bars'},'Orientation','horizontal','Location','SouthEast');
 ylabel ('Cp')
 
 set(gca, 'YDir','reverse')
@@ -335,9 +427,13 @@ plot(x_c(:,1),zeros(1,length(Cp(:,i))),'k')
 patch([x_c(:,1), fliplr(x_c(:,1))], [Cp(:,i) fliplr(Cp(:,i))], [0.9290, 0.6940, 0.1250], 'FaceAlpha',0.8)
 alpha(0.1); %change transperancy of  the filling
 
+hold on
+errorbar(x_c(:,1),Cp(:,i),Cp_max(:,i),Cp_min(:,i))
+hold on
 grid minor
 title(['\alpha = ' num2str(Alpha_V(1,i)) ', V = ' num2str(Alpha_V(2,i))]);
 xlabel( ' x/c ')
+legend({'Cp Values','Linear connection','Area under the curve','Error bars'},'Orientation','horizontal','Location','SouthEast');
 ylabel ('Cp')
 
 set(gca, 'YDir','reverse')
@@ -357,9 +453,14 @@ plot(x_c(:,1),zeros(1,length(Cp(:,i))),'k')
 patch([x_c(:,1), fliplr(x_c(:,1))], [Cp(:,i) fliplr(Cp(:,i))], [0.9290, 0.6940, 0.1250], 'FaceAlpha',0.8)
 alpha(0.1); %change transperancy of  the filling
 
+hold on
+errorbar(x_c(:,1),Cp(:,i),Cp_max(:,i),Cp_min(:,i))
+hold on
+
 grid minor
 title(['\alpha = ' num2str(Alpha_V(1,i)) ', V = ' num2str(Alpha_V(2,i))]);
 xlabel( ' x/c ')
+legend({'Cp Values','Linear connection','Area under the curve','Error bars'},'Orientation','horizontal','Location','SouthEast');
 ylabel ('Cp')
 
 set(gca, 'YDir','reverse')
@@ -376,73 +477,80 @@ scatter(Alpha_V(1,:),(Cl./Cd))
 grid minor
 title(['\alpha Vs L/D']);
 
-%% error analysis: Cp
+a = 1;
+%% error analysis: Cp: Not needed.
 
 % uncertainties values are the same like the previous lab, check lab report
 % to see how those are done.
 
 
-SigmaTemp = 0.25 ; % in k
-SigmaatmPressure = (250-20)*10^3*(1.5/100); %from lab document
-SigmaDiffPressure = 6894.76 * (1/100); %from lab document
-SgimaScav = 0.20;
-% get error fro v_inifity
-Rfluid = 287;
-
-% Use pitot function
-[ Velocinf Errorinf ] = Pitot (PatmMean, TatmMean, pitotDynamicPMean, SigmaatmPressure, SigmaTemp, SigmaDiffPressure,Rfluid);
-
-
-
-% now get the error for Cp:
-
-
-syms Pscav Patmo Tatmo V_freeStream
-
-Cp_error_eqn(Pscav,Patmo,Tatmo,V_freeStream) = Pscav / ((0.5)*(Patmo/(Rfluid*Tatmo))*(V_freeStream));
-
-% get partial derivatives
-
-Cp_Partial_Pscav = diff(Cp_error_eqn,Pscav);
-Cp_Partial_Patmo = diff(Cp_error_eqn,Patmo);
-Cp_Partial_Tatmo = diff(Cp_error_eqn,Tatmo);
-Cp_Partial_V_freeStream = diff(Cp_error_eqn,V_freeStream);
-
-
-% if there's constant uncertinity values, create a matrix with their
-% size
-    
-
-% if length(SigmaDiffPressure) == 1 && length(SigmaTemp)==1 && length(SigmaatmPressure) == 1
-%     SigmaDiffPressure = ones(1,length(P_atm)) .* SigmaDiffPressure;
-%     SigmaTemp = ones(1,length(P_atm)) .* SigmaDiffPressure;
-%     SigmaatmPressure = ones(1,length(P_atm)) .* SigmaDiffPressure;
+% SigmaTemp = 0.25 ; % in k
+% SigmaatmPressure = (250-20)*10^3*(1.5/100); %from lab document
+% SigmaDiffPressure = 6894.76 * (1/100); %from lab document
+% SgimaScav = 0.20;
+% % get error fro v_inifity
+% Rfluid = 287;
 % 
-% else
+% % Use pitot function
+% [ Velocinf Errorinf ] = Pitot (PatmMean, TatmMean, pitotDynamicPMean, SigmaatmPressure, SigmaTemp, SigmaDiffPressure,Rfluid);
+% 
+% 
+% 
+% % now get the error for Cp:
+% 
+% 
+% syms Pscav Patmo Tatmo V_freeStream
+% 
+% Cp_error_eqn(Pscav,Patmo,Tatmo,V_freeStream) = Pscav / ((0.5)*(Patmo/(Rfluid*Tatmo))*(V_freeStream));
+% 
+% % get partial derivatives
+% 
+% Cp_Partial_Pscav = diff(Cp_error_eqn,Pscav);
+% Cp_Partial_Patmo = diff(Cp_error_eqn,Patmo);
+% Cp_Partial_Tatmo = diff(Cp_error_eqn,Tatmo);
+% Cp_Partial_V_freeStream = diff(Cp_error_eqn,V_freeStream);
+% 
+% 
+% % if there's constant uncertinity values, create a matrix with their
+% % size
 %     
-%     SigmaDiffPressure = sigma_Air_P_Diff;
-%     SigmaTemp = SigmaTemp;
-%     SigmaatmPressure = SigmaatmPressure;
-%     
+% 
+% % if length(SigmaDiffPressure) == 1 && length(SigmaTemp)==1 && length(SigmaatmPressure) == 1
+% %     SigmaDiffPressure = ones(1,length(P_atm)) .* SigmaDiffPressure;
+% %     SigmaTemp = ones(1,length(P_atm)) .* SigmaDiffPressure;
+% %     SigmaatmPressure = ones(1,length(P_atm)) .* SigmaDiffPressure;
+% % 
+% % else
+% %     
+% %     SigmaDiffPressure = sigma_Air_P_Diff;
+% %     SigmaTemp = SigmaTemp;
+% %     SigmaatmPressure = SigmaatmPressure;
+% %     
+% % end
+% 
+% 
+% % loop to get all errors: one 
+% % this will be loop for each speed/angel of attack once.
+% 
+% [ r c ] = size(Cp);
+% for j=1:c
+%     for i=1:r-1
+%         
+%  value_before_root(i,j) = ( ((Cp_Partial_Pscav( SP_All_Updated(j,i) ,PatmMean(j),TatmMean(j),Velocinf(j)))*SgimaScav)^2 ...
+%  + ((Cp_Partial_Patmo(SP_All_Updated(j,i),PatmMean(j),TatmMean(j),Velocinf(j)))*SigmaatmPressure)^2 ...
+%  +((Cp_Partial_Tatmo(SP_All_Updated(j,i),PatmMean(j),TatmMean(j),Velocinf(j)))*SigmaTemp)^2 ...
+%  +((Cp_Partial_V_freeStream(SP_All_Updated(j,i),PatmMean(j),TatmMean(j),Velocinf(j)))*Errorinf(j))^2 ) ;
+%  
+%  Erros_Cp(i,j) = double(sqrt(value_before_root(i,j)));
+% 
+%     end
 % end
-
-
-% loop to get all errors: one 
-% this will be loop for each speed/angel of attack once.
-
-[ r c ] = size(Cp);
-for j=1:c
-    for i=1:r-1
-        
- value_before_root(i,j) = ( ((Cp_Partial_Pscav( SP_All_Updated(j,i) ,PatmMean(j),TatmMean(j),Velocinf(j)))*SgimaScav)^2 ...
- + ((Cp_Partial_Patmo(SP_All_Updated(j,i),PatmMean(j),TatmMean(j),Velocinf(j)))*SigmaatmPressure)^2 ...
- +((Cp_Partial_Tatmo(SP_All_Updated(j,i),PatmMean(j),TatmMean(j),Velocinf(j)))*SigmaTemp)^2 ...
- +((Cp_Partial_V_freeStream(SP_All_Updated(j,i),PatmMean(j),TatmMean(j),Velocinf(j)))*Errorinf(j))^2 ) ;
- 
- Erros_Cp(i,j) = double(sqrt(value_before_root(i,j)));
-
-    end
-end
-
-a = 1;
-
+% 
+% % now we have to do the last port error, which is combinations of two
+% % edges
+% 
+% % the Error_Cp has 16*12, each column is one test, each row is one of the
+% % ports in order.
+% 
+% % the trailing edge has 
+% 
